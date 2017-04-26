a6d42cff32167a2a4884307a2f30ff55f29c9b08
Meheret Egzerab <meheretgirma22@gmail.com>
2017-04-26 17:32:07 -0400

    add the smooth scroll

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -1,2 +1,11 @@
+var app = angular.module("BookApp");
 
+app.controller("PageScrollCtrl", ["$scope", "$location", "$anchorScroll",function($scope, $location, $anchorScroll){
+    $scope.gotoMiddle = function(){
+        $location.hash('second');
+       
+        $anchorScroll()
+    }
+}])
+     
 
