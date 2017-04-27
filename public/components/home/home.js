763c52df86ce148f6cad0d004d7fe70657122aa4
Meheret Egzerab <meheretgirma22@gmail.com>
2017-04-27 17:17:01 -0400

    center the front page

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -3,7 +3,6 @@ var app = angular.module("BookApp");
 app.controller("PageScrollCtrl", ["$scope", "$location", "$anchorScroll",function($scope, $location, $anchorScroll){
     $scope.gotoMiddle = function(){
         $location.hash('second');
-       
         $anchorScroll()
     }
 }])
